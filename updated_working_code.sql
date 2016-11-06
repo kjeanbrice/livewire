9f216ce48be1be7a1cf64a86a0db3e059ad81c37
Kenji <kenji.japra@gmail.com>
2016-11-06 14:14:16 -0500

    Fixed indentation

--- updated_working_code.sql
+++ updated_working_code.sql
@@ -1,69 +1,68 @@
 CREATE TABLE user_data(
 	user_id INT NOT NULL AUTO_INCREMENT,
-    last_name VARCHAR(30),
-    first_name VARCHAR(30),
-    address VARCHAR(50),
-    city VARCHAR(30),
-    state CHAR(2),
-    zip_code INT,
-    telephone INT,
-    email VARCHAR(50),
-    account_number INT NOT NULL,
-    creation_date DATE,
+    	last_name VARCHAR(30),
+    	first_name VARCHAR(30),
+    	address VARCHAR(50),
+    	city VARCHAR(30),
+   	state CHAR(2),
+    	zip_code INT,
+    	telephone INT,
+    	email VARCHAR(50),
+    	account_number INT NOT NULL,
+    	creation_date DATE,
 	credit_card BIGINT DEFAULT NULL,
-    rating INT,
-    PRIMARY KEY(user_id),
-    UNIQUE(telephone),
+    	rating INT,
+    	PRIMARY KEY(user_id),
+    	UNIQUE(telephone),
 	UNIQUE(email),
-    UNIQUE(account_number)
+    	UNIQUE(account_number)
 );
 
 CREATE TABLE user_preferences(
 	user_id INT NOT NULL,
-    preference VARCHAR(20),
-    FOREIGN KEY(user_id) REFERENCES user_data(user_id)
+    	preference VARCHAR(20),
+    	FOREIGN KEY(user_id) REFERENCES user_data(user_id)
 		ON DELETE CASCADE
 		ON UPDATE CASCADE
 );
 
 CREATE TABLE group_data(
-	group_id		INT 	NOT NULL AUTO_INCREMENT,
-    user_id			INT		NOT NULL,
-    group_name		VARCHAR(50)	NOT NULL,
-    group_type		VARCHAR(50) NOT NULL,
-    creation_date	TIMESTAMP	NOT NULL,
-    
-    PRIMARY KEY(group_id),
-    FOREIGN KEY(user_id) REFERENCES user_data(user_id)
+	group_id INT NOT NULL AUTO_INCREMENT,
+    	user_id INT NOT NULL,
+    	group_name VARCHAR(50) NOT NULL,
+    	group_type VARCHAR(50) NOT NULL,
+    	creation_date TIMESTAMP NOT NULL,
+    	PRIMARY KEY(group_id),
+    	FOREIGN KEY(user_id) REFERENCES user_data(user_id)
 		ON DELETE CASCADE
-        ON UPDATE CASCADE
+        	ON UPDATE CASCADE
 );
 
 CREATE TABLE group_members(
-	group_id	INT		NOT NULL,
-    user_id		INT		NOT NULL,
-    join_date	TIMESTAMP	NOT NULL,
-    PRIMARY KEY(group_id,user_id),
+	group_id INT NOT NULL,
+    	user_id INT NOT NULL,
+    	join_date TIMESTAMP NOT NULL,
+    	PRIMARY KEY(group_id,user_id),
 	FOREIGN KEY(user_id) REFERENCES user_data(user_id)
 		ON DELETE CASCADE
-        ON UPDATE CASCADE,
+        	ON UPDATE CASCADE,
 	FOREIGN KEY(group_id) REFERENCES group_data(group_id)
 		ON DELETE CASCADE
-        ON UPDATE CASCADE
+        	ON UPDATE CASCADE
 );
 
 CREATE TABLE page_data(
 	page_id INT NOT NULL AUTO_INCREMENT,
-    page_owner INT,
-    associated_group INT,
-    post_count INT,
-    PRIMARY KEY(page_id),
-    FOREIGN KEY(page_owner) REFERENCES user_data(user_id)
+    	page_owner INT,
+    	associated_group INT,
+    	post_count INT,
+    	PRIMARY KEY(page_id),
+    	FOREIGN KEY(page_owner) REFERENCES user_data(user_id)
 		ON DELETE CASCADE
-        ON UPDATE CASCADE,
+        	ON UPDATE CASCADE,
 	FOREIGN KEY(associated_group) REFERENCES group_data(group_id)
 		ON DELETE CASCADE
-        ON UPDATE CASCADE
+        	ON UPDATE CASCADE
 );
 
 CREATE TABLE post_data
@@ -75,39 +74,39 @@ CREATE TABLE post_data
 	commentCount Integer,
 	PRIMARY KEY(post_id),
 	FOREIGN KEY(user_id) REFERENCES user_data(user_id)
-		ON DELETE CASCADE
+	ON DELETE CASCADE
         ON UPDATE CASCADE
 );
 
 CREATE TABLE comment_data(
 	comment_id INT NOT NULL AUTO_INCREMENT,
-    post_id INT NOT NULL,
-    user_id INT NOT NULL,
-    comment_content VARCHAR(250) NOT NULL,
-    creation_date TIMESTAMP NOT NULL,
-    PRIMARY KEY(comment_id),
-    FOREIGN KEY(user_id) REFERENCES user_data(user_id)
+    	post_id INT NOT NULL,
+    	user_id INT NOT NULL,
+    	comment_content VARCHAR(250) NOT NULL,
+    	creation_date TIMESTAMP NOT NULL,
+	PRIMARY KEY(comment_id),
+    	FOREIGN KEY(user_id) REFERENCES user_data(user_id)
 		ON DELETE CASCADE
-        ON UPDATE CASCADE,
+        	ON UPDATE CASCADE,
 	FOREIGN KEY(post_id) REFERENCES post_data(post_id)
 		ON DELETE CASCADE
-        ON UPDATE CASCADE
+        	ON UPDATE CASCADE
 );
 
 CREATE TABLE messages_data(
 	message_id INT NOT NULL AUTO_INCREMENT,
-    message_date DATE, 
-    message_subject VARCHAR(50),
-    message_content VARCHAR(511), -- 511 because 512 will add an aditional byte and decrease efficiency
-    message_sender INT NOT NULL,
-    message_receiver INT NOT NULL,
-    PRIMARY KEY(message_id),
-    FOREIGN KEY(message_sender) REFERENCES user_data(user_id)
+    	message_date DATE, 
+    	message_subject VARCHAR(50),
+    	message_content VARCHAR(511), -- 511 because 512 will add an aditional byte and decrease efficiency
+    	message_sender INT NOT NULL,
+	message_receiver INT NOT NULL,
+    	PRIMARY KEY(message_id),
+    	FOREIGN KEY(message_sender) REFERENCES user_data(user_id)
 		ON DELETE CASCADE
-        ON UPDATE CASCADE,
+        	ON UPDATE CASCADE,
 	FOREIGN KEY(message_receiver) REFERENCES user_data(user_id)
 		ON DELETE CASCADE
-        ON UPDATE CASCADE
+        	ON UPDATE CASCADE
 );
 
 CREATE TABLE employee_data(
@@ -126,7 +125,7 @@ CREATE TABLE employee_data(
 
 CREATE TABLE advertisement_data(
 	advertisement_id INT NOT NULL AUTO_INCREMENT,
-	employee_id	 INT NOT NULL,
+	employee_id INT NOT NULL,
 	date_of_ad DATE NOT NULL,
 	type_of_ad DATE NOT NULL,
 	company VARCHAR(50) NOT NULL,
@@ -137,25 +136,25 @@ CREATE TABLE advertisement_data(
 	PRIMARY KEY(advertisement_id),
 	FOREIGN KEY(employee_id) REFERENCES employee_data(social)
 		ON DELETE CASCADE
-        ON UPDATE CASCADE
+        	ON UPDATE CASCADE
 );
 
 CREATE TABLE sale_data(
 	transaction_id INT NOT NULL AUTO_INCREMENT,
 	ad_id INT NOT NULL,
-    seller_id INT NOT NULL,
-    consumer_id INT NOT NULL,
-    transaction_date DATE NOT NULL,
-    number_of_units INT NOT NULL,
-    account_number INT NOT NULL, 
-    PRIMARY KEY(transaction_id),
-    FOREIGN KEY(seller_id) REFERENCES user_data(user_id)
+    	seller_id INT NOT NULL,
+    	consumer_id INT NOT NULL,
+    	transaction_date DATE NOT NULL,
+    	number_of_units INT NOT NULL,
+    	account_number INT NOT NULL, 
+    	PRIMARY KEY(transaction_id),
+    	FOREIGN KEY(seller_id) REFERENCES user_data(user_id)
 		ON DELETE CASCADE
-        ON UPDATE CASCADE,
+        	ON UPDATE CASCADE,
 	FOREIGN KEY(consumer_id) REFERENCES user_data(user_id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE,
+        	ON DELETE CASCADE
+        	ON UPDATE CASCADE,
 	FOREIGN KEY(ad_id) REFERENCES advertisement_data(advertisement_id)
 		ON DELETE CASCADE
-        ON UPDATE CASCADE
+        	ON UPDATE CASCADE
 );
